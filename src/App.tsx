9ff5f50d2f3e4f2faa21ae970d4f661a3d8c9d3a
Alan Russell <ajrussellaudio@gmail.com>
2024-09-22 18:37:11 +0100

    Install Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import "@fontsource-variable/inter";
+import "./App.css";
+
 const App = () => {
   return (
-      <div>
-          <h1>Hello Twinkl!</h1>
-      </div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello Twinkl!</h1>
+    </div>
+  );
+};
 
 export default App;
